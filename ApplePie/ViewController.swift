4527f861575db4a358fc85af0301171dd801bc8f
Vadim Colcev <vadimkolchev@gmail.com>
2020-12-05 00:43:37 +0200

    Initial Commit

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Vadim Colcev on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
